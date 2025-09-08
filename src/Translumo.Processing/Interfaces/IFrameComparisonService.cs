ec884fe0ebf6312517d56db750a360a54984cd82
wgjuher <vladislav.kipnis.work@yandex.ru>
2025-09-08 22:00:41 +0300

    fix error in TranslationProcessingService

--- src/Translumo.Processing/Interfaces/IFrameComparisonService.cs
+++ src/Translumo.Processing/Interfaces/IFrameComparisonService.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace Translumo.Processing.Interfaces
 {
     /// <summary>
     /// Service for comparing captured frames to determine if translation should be triggered.
     /// Only processes translation when frames have been stable for a configured number of iterations.
     /// </summary>
-    public interface IFrameComparisonService
+    public interface IFrameComparisonService : IDisposable
     {
         /// <summary>
         /// Determines if the current frame should trigger OCR and translation processing.
